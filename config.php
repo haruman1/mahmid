f7d36f296a43ef72f4c067c1636a2eb86007dd8b
haruman1 <30350640+haruman1@users.noreply.github.com>
2018-11-26 04:27:16 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+///Jangan hapus tanpa kutip nya(")
+
+///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
+
+///Ganti xxxxxxx dengan data punya kalian
+
+///Capture dulu apk nya ambil data yang di butuhkan
+
+$ticket = "xxxxxxxxxxxxxxxxxx";
+
+$meid = "xxxxxxxxxxxxxxxxxx";
+
+$os="xxxxxxxxxxxxxxxxxx";
+
+$sign="xxxxxxxxxxxxxxxxxx";
