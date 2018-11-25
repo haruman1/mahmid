7c08fea1f3e6a9c72ef5faf267f0fd617e721107
haruman1 <30350640+haruman1@users.noreply.github.com>
2018-11-26 05:11:21 +0700

    Update config.php

--- config.php
+++ config.php
@@ -8,10 +8,10 @@
 
 ///Capture dulu apk nya ambil data yang di butuhkan
 
-$ticket = "xxxxxxxxxxxxxxxxxx";
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBh3-Xu72KocC1ncu9jYqxfmSny4Pcl2C1dYqehaO724t3h5a7qoagwLiKlK2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
 
-$meid = "xxxxxxxxxxxxxxxxxx";
+$meid = "70a79c25e0973d4c";
 
-$os="xxxxxxxxxxxxxxxxxx";
+$os="android";
 
-$sign="xxxxxxxxxxxxxxxxxx";
+$sign="34946587955d349f9563e7d68bd1f2824dee67dc21d644ba9c7e7d58796366f7";
