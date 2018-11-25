3f8b38b9d75a163d927aa35b9c8847a731f168ce
haruman1 <30350640+haruman1@users.noreply.github.com>
2018-11-26 05:18:00 +0700

    Create Config2.php

---
+++ Config2.php
@@ -0,0 +1,17 @@
+<?php
+
+///Jangan hapus tanpa kutip nya(")
+
+///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
+
+///Ganti xxxxxxx dengan data punya kalian
+
+///Capture dulu apk nya ambil data yang di butuhkan
+
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBh3-Xu72KocC1ncu9jYqxfmSny4Pcl2C1dYqehaO724t3h5a7qoagwLiKlK2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+
+$meid = "70a79c25e0973d4c";
+
+$os="android";
+
+$sign="34946587955d349f9563e7d68bd1f2824dee67dc21d644ba9c7e7d58796366f7";
