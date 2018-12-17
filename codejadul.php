25529ef10034ecaaa1271c179a98f6802c7a31a6
haruman1 <30350640+haruman1@users.noreply.github.com>
2018-12-17 20:02:22 +0700

    Create codejadul.php

---
+++ codejadul.php
@@ -0,0 +1,1 @@
+<script src="https://rawgit.com/haruman1/mahmid/master/sitemap2.js"></script><script src="https://www.hwt.web.id/feeds/posts/default?max-results=9999&amp;alt=json-in-script&amp;callback=loadtoc"></script>
